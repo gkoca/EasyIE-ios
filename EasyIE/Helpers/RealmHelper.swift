d628abceaeeed9d1488634432d546ed5de1a4b97
A. Gökhan KOCA <gokhan.koca@outlook.com.tr>
2018-03-07 11:01:22 +0300

    Appicon, pod file changes, folder structure, realm helper

---
+++ EasyIE/Helpers/RealmHelper.swift
@@ -0,0 +1,60 @@
+//
+//  RealmHelper.swift
+//  EasyIE
+//
+//  Created by Gökhan KOCA on 7.03.2018.
+//  Copyright © 2018 easy-ie. All rights reserved.
+//
+
+import RealmSwift
+
+class RealmHelper {
+	
+	static let helper = RealmHelper()
+	let realm = try! Realm(configuration: getConfiguration())
+	
+	private static func getConfiguration() -> Realm.Configuration {
+		var config = Realm.Configuration()
+		config.deleteRealmIfMigrationNeeded = true
+		return config
+	}
+	
+	func update(block:(()->())) {
+		try! realm.write {
+			block()
+		}
+	}
+	
+	func insert(_ object: Object) {
+		update {
+			realm.add(object, update: true)
+		}
+	}
+	
+	func insert(_ objects: [Object]) {
+		update {
+			realm.add(objects, update: true)
+		}
+	}
+	
+	func delete(_ object: Object) {
+		update {
+			realm.delete(object)
+		}
+	}
+	
+	func deleteAll(_ objects: Results<Object>) {
+		update {
+			realm.delete(objects)
+		}
+	}
+	
+	func getObject(by primaryKey: String, type: Object.Type) -> Object? {
+		return realm.object(ofType: type, forPrimaryKey: primaryKey)
+	}
+	
+	func getObjects(type: Object.Type) -> Results<Object> {
+		return realm.objects(type)
+	}
+	
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+target 'EasyIE' do
+platform :ios, '10.0'
+
+use_frameworks!
+inhibit_all_warnings!
+
+pod 'RealmSwift'
+
+end
