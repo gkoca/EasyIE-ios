d9988eceadeee2c8f4e6fdaf5b4f3f2612b3ba34
A. Gökhan KOCA <gokhan.koca@outlook.com.tr>
2018-03-07 09:54:13 +0300

    Created project

---
+++ EasyIE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyIE
+//
+//  Created by Gökhan KOCA on 7.03.2018.
+//  Copyright © 2018 easy-ie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
